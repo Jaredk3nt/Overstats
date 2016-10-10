d908b5773e95b5915d2ebb518b790a7683900528
Jared Jones <jkjones.dev@gmail.com>
2016-10-10 17:47:07 -0500

    closes #2, not final design but good working state, changed the way the selectors pull from the map and outcome on the add match form

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // ENVIRONMENT SETUP
 // MAP object, works as ENUM for map types and names
+/*
 var MAP = {
 	EICHENWALD 	: {value: 0, name: "Eichenwald", code: "Hybrid"},
   	KINGSROW   	: {value: 1, name: "King's Row", code: "Hybrid"},
@@ -18,17 +19,32 @@ var MAP = {
 	ANUBIS		: {value: 11, name: "Temple of Anubis", code: "Assault"},
 	HANAMURA	: {value: 12, name: "Hanamura", code: "Assault"}
 };
+*/
+var MAP = [
+	{value: 0, name: "Eichenwald", code: "Hybrid"},
+  	{value: 1, name: "King's Row", code: "Hybrid"},
+	{value: 2, name: "Hollywood", code: "Hybrid"},
+	{value: 3, name: "Numbani", code: "Hybrid"},
+	
+	{value: 4, name: "Ilios", code: "ControlPoint"},
+	{value: 5, name: "Lijiang Tower", code: "ControlPoint"},
+	{value: 6, name: "Nepal", code: "ControlPoint"},
+	
+	{value: 7, name: "Dorado", code: "Payload"},
+	{value: 8, name: "Route 66", code: "Payload"},
+	{value: 9, name: "Watchpoint Gibraltar", code: "Payload"},
+	
+	{value: 10, name: "Volskaya Industries", code: "Assault"},
+	{value: 11, name: "Temple of Anubis", code: "Assault"},
+	{value: 12, name: "Hanamura", code: "Assault"}
+];
 
 // OUTCOME object, works as ENUM for outcome types
-var OUTCOME = {
-    WIN   : {value: 1, name: "Victory"},
-    LOSS  : {value: 0, name: "Defeat"},
-    TIE   : {value: 2, name: "Draw"}
-};
-
-// Initialize the enum lists for the selectors
-let mapNames = Object.keys(MAP);
-let outcomeTypes = Object.keys(OUTCOME);
+var OUTCOME = [
+    {value: 1, name: "Victory"},
+    {value: 0, name: "Defeat"},
+    {value: 2, name: "Draw"}
+];
 
 // Match object used to store individual match data
 function match(m, sr, o, d) {
@@ -41,10 +57,9 @@ function match(m, sr, o, d) {
 // Angular app init
 var OverwatchStats = angular.module('OverwatchStats', []);
 
-
 // TEST CODE
-var m1 = new match(MAP.EICHENWALD, 2500, OUTCOME.WIN, "10/07/16");
-var m2 = new match(MAP.ANUBIS, 2434, OUTCOME.LOSS, "10/07/16");
+var m1 = new match(MAP[0], 2500, OUTCOME.WIN, "10/07/16");
+var m2 = new match(MAP[11], 2434, OUTCOME.LOSS, "10/07/16");
 var matchesList = [];
 matchesList.push(m2);
 
@@ -52,11 +67,12 @@ matchesList.push(m2);
 OverwatchStats.controller('matchListController', 
                           function matchListController($scope) {
 	$scope.matches = matchesList;
-    $scope.maps = mapNames;
-    $scope.outcomes = outcomeTypes;
+    $scope.maps = MAP;
+    $scope.outcomes = OUTCOME;
   
   	$scope.addMatch = function() {
 	  	// grab values from modal
+        console.log($scope.newMap);
 		var m = new match($scope.newMap, $scope.newSR, $scope.newOutcome, "");
     	$scope.matches.push(m);
         console.log(matches);

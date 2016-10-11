baad2f5e1469eb8c2ce10f58aeab372fcfce7f65
Jared Jones <jkjones.dev@gmail.com>
2016-10-11 09:51:19 -0500

    closes #3, site now holds onto all input matches using localStorage.

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 // ENVIRONMENT SETUP
 // MAP object, works as ENUM for map types and names
-/*
-var MAP = {
-	EICHENWALD 	: {value: 0, name: "Eichenwald", code: "Hybrid"},
-  	KINGSROW   	: {value: 1, name: "King's Row", code: "Hybrid"},
-	HOLLYWOOD  	: {value: 2, name: "Hollywood", code: "Hybrid"},
-	NUMBANI    	: {value: 3, name: "Numbani", code: "Hybrid"},
-	
-	ILIOS		: {value: 4, name: "Ilios", code: "ControlPoint"},
-	LIJIANG		: {value: 5, name: "Lijiang Tower", code: "ControlPoint"},
-	NEPAL		: {value: 6, name: "Nepal", code: "ControlPoint"},
-	
-	DORADO		: {value: 7, name: "Dorado", code: "Payload"},
-	ROUTE		: {value: 8, name: "Route 66", code: "Payload"},
-	GIBRALTAR	: {value: 9, name: "Watchpoint Gibraltar", code: "Payload"},
-	
-	VOLSKAYA	: {value: 10, name: "Volskaya Industries", code: "Assault"},
-	ANUBIS		: {value: 11, name: "Temple of Anubis", code: "Assault"},
-	HANAMURA	: {value: 12, name: "Hanamura", code: "Assault"}
-};
-*/
 var MAP = [
 	{value: 0, name: "Eichenwald", code: "Hybrid"},
   	{value: 1, name: "King's Row", code: "Hybrid"},
@@ -56,25 +36,60 @@ function match(m, sr, o, d) {
 
 // Angular app init
 var OverwatchStats = angular.module('OverwatchStats', []);
+var localStorageAvaliable = false;
 
 // TEST CODE
-var m1 = new match(MAP[0], 2500, OUTCOME[1], "10/07/16");
-var m2 = new match(MAP[11], 2434, OUTCOME[2], "10/07/16");
 var matchesList = [];
-matchesList.push(m2);
+
+
+
+window.onload = function () {
+ 	//localStorage.clear();
+};
+
+function loadFromStorage() {
+	var storedJSON = localStorage.getItem('matches');
+	if(storedJSON !== null) {
+		var data = JSON.parse(storedJSON);
+		console.log(storedJSON);
+		//console.log(data[2].map.name);
+		return data;
+	}
+	return [];
+}
+
+function addMatchLS(m) {
+	var storedJSON = localStorage.getItem('matches');
+	var matches = [];
+	if(storedJSON !== null) {
+		matches = JSON.parse(storedJSON);
+	}
+
+    matches.push(m);
+    localStorage.setItem('matches', JSON.stringify(matches));
+}
 
 // ANGULAR LIST CONTROLLER
 OverwatchStats.controller('matchListController', 
                           function matchListController($scope) {
-	$scope.matches = matchesList;
+	$scope.matches = [];
+	if (typeof(Storage) !== "undefined") {
+		localStorageAvaliable = true;
+		$scope.matches = loadFromStorage();
+	}
+	if($scope.matches === null){
+		$scope.matches = [];
+	}
+	
     $scope.maps = MAP;
     $scope.outcomes = OUTCOME;
   
   	$scope.addMatch = function() {
 	  	// grab values from modal
-        console.log($scope.newMap);
 		var m = new match($scope.newMap, $scope.newSR, $scope.newOutcome, "");
-    	$scope.matches.push(m);
-        console.log(matches);
+		$scope.matches.push(m);
+		addMatchLS(m);
   }
-})
+});
+
+//{"matches": [ {"map": x, "skillrating": x, "outcome": x, "date": x}, {...}, ... ]}
